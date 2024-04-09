72e15c42148117009826e74d5fb829517a64a665
yeyechen <yc1319@imperial.ac.uk>
2024-04-09 16:50:33 +0800

    add constants; add output module

--- yang_zhang.py
+++ yang_zhang.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 import os
-from datetime import datetime
 from math import *
 
 CMT = 20
+ALPHA = 1.34
+TRADING_DAYS_IN_A_YEAR = 244
 
 
 def yang_zhang(daily_data):
@@ -16,41 +17,45 @@ def yang_zhang(daily_data):
         sigma: Yang Zhang volatility at current time
     """
 
-    # set C_0 as O_1, shift close data to get ln_open_data
-    shifted_select_data_until_time = daily_data['close']
-    shifted_select_data_until_time.loc[-1] = daily_data.loc[0, 'open']
-    shifted_select_data_until_time.index += 1
-    shifted_select_data_until_time.sort_index(inplace=True)
-
     # perform row operation to get ln_open/high/low/close_data
-    daily_data['ln_open'] = np.log(daily_data['high'] / shifted_select_data_until_time)
-    daily_data['ln_high'] = np.log(daily_data['high'] / daily_data['open'])
-    daily_data['ln_low'] = np.log(daily_data['low'] / daily_data['open'])
-    daily_data['ln_close'] = np.log(daily_data['close'] / daily_data['open'])
-    ln_open_data = daily_data.loc[:, 'ln_open']
-    ln_high_data = daily_data.loc[:, 'ln_high']
-    ln_low_data = daily_data.loc[:, 'ln_low']
-    ln_close_data = daily_data.loc[:, 'ln_close']
+    ln_open_data = np.log(daily_data['open'] / daily_data['shifted_close'])
+    ln_high_data = np.log(daily_data['high'] / daily_data['open'])
+    ln_low_data = np.log(daily_data['low'] / daily_data['open'])
+    ln_close_data = np.log(daily_data['close'] / daily_data['open'])
 
     # calculate Yang Zhang volatility
     ln_open_mean = sum(ln_open_data) / CMT
     ln_close_mean = sum(ln_close_data) / CMT
-    V_o = sum((o_i - ln_open_mean) ** 2 for o_i in ln_open_data) / (CMT - 1)
-    V_c = sum((c_i - ln_close_mean) ** 2 for c_i in ln_close_data) / (CMT - 1)
-    V_rs = sum(ln_high_data * (ln_high_data - ln_close_data) + ln_low_data * (ln_low_data - ln_close_mean)) / CMT
-    k = 0.34 / (1.34 + (CMT + 1) / (CMT - 1))
+    V_o = sum((o_i - ln_open_mean) ** 2 for o_i in ln_open_data) * TRADING_DAYS_IN_A_YEAR / (CMT - 1)
+    V_c = sum((c_i - ln_close_mean) ** 2 for c_i in ln_close_data) * TRADING_DAYS_IN_A_YEAR / (CMT - 1)
+    V_rs = sum(ln_high_data * (ln_high_data - ln_close_data) + ln_low_data * (ln_low_data - ln_close_data)) * TRADING_DAYS_IN_A_YEAR / CMT
+    k = (ALPHA - 1) / (ALPHA + (CMT + 1) / (CMT - 1))
     sigma = sqrt(V_o + k * V_c + (1 - k) * V_rs)
 
     return sigma
 
 
 def main():
-    yz_volatility =[]
+    yz_volatility = []
     trading_time = list(etf_daily_data.iloc[CMT-1:].loc[:, 'time'])
+
+    # set C_0 as O_1, shift down close data
+    shifted_close_data_until_time = etf_daily_data['close']
+    shifted_close_data_until_time.loc[-1] = etf_daily_data.loc[0, 'open']
+    shifted_close_data_until_time.index += 1
+    shifted_close_data_until_time.sort_index(inplace=True)
+    etf_daily_data['shifted_close'] = shifted_close_data_until_time
 
     for time in trading_time:
         select_data_until_time = etf_daily_data[etf_daily_data['time'] <= time].tail(CMT).reset_index(drop=True)
-        yz_volatility.append(yang_zhang(select_data_until_time))
+        yz_volatility.append(100*yang_zhang(select_data_until_time))
+
+    # output to a csv file
+    output = pd.DataFrame({'trading_time': trading_time, 'yang_zhang': yz_volatility})
+    save_folder = 'calc_data'
+    output_name = 'etf_2020_yang_zhang_daily.csv'
+    save_path = os.path.join(global_path, save_folder, output_name)
+    output.to_csv(save_path, index=False)
 
 
 if __name__ == '__main__':
@@ -66,6 +71,5 @@ if __name__ == '__main__':
     etf_daily_data = etf_daily_data.rename(columns={'OPEN': 'open'})
     selected_columns = ['time', 'open', 'high', 'low', 'close']
     etf_daily_data = etf_daily_data[selected_columns].reset_index(drop=True)
-
 
     main()
